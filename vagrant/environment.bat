27e19139a1f1db0df55d42be9d6222c658be228a
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-11-13 08:48:35 -0400

    add vagrant file and environment

---
+++ vagrant/environment.bat
@@ -0,0 +1,12 @@
+@echo off
+set OS_AUTH_URL=http://10.28.3.240:5000/v3
+set OS_IDENTITY_API_VERSION=3
+set OS_TENANT_NAME=f38a9fab5ce549bcb2d52f5cbe67ac3a
+set OS_PROJECT_NAME=fj_franzsilva_at07-desktop
+set OS_USERNAME=FranzSilva
+set OS_PASSWORD=f06112018S
+set OS_DOMAIN_NAME=fundacion-jala
+set OS_FLAVOR=f2.desktop.small
+set OS_IMAGE=ubuntu-16.04-amd64-server_18062018
+set OS_KEY_PAIR_NAME=franzDevOps
+set OS_REGION_NAME=bootstack-jalasoft
