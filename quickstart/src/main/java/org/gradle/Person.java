d7eec011507d56c073e166d33f00a0240c8f1e09
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-11-15 08:59:07 -0400

    add unit test and add line isntall java vangrat file

--- quickstart/src/main/java/org/gradle/Person.java
+++ quickstart/src/main/java/org/gradle/Person.java
@@ -1,16 +1,134 @@
 package org.gradle;
 
-import org.apache.commons.collections.list.GrowthList;
-
+/**
+ *
+ *this class person
+ */
 public class Person {
     private final String name;
+    private final int age;
+    private final String ci;
+    private final double heightMts;
+    private final double weightKg;
+    private final String phone;
+    private final String address;
+    private final String gender;
+    private final String email;
+    private final String nationality;
+    private final String civilStatus;
 
-    public Person(String name) {
+    /**
+     * Constructor for Person class.
+     *
+     * @param name
+     * @param age
+     * @param ci
+     * @param heightMts
+     * @param weightKg
+     * @param phoneNumber
+     * @param address
+     * @param gender
+     * @param email
+     * @param nationality
+     * @param civilStatus
+     */
+    public Person(String name, int age, String ci, double heightMts, double weightKg,
+                  String phoneNumber, String address, String gender, String email,
+                  String nationality, String civilStatus) {
         this.name = name;
-        new GrowthList();
+        this.age = age;
+        this.ci = ci;
+        this.heightMts = heightMts;
+        this.weightKg = weightKg;
+        this.phone = phoneNumber;
+        this.address = address;
+        this.gender = gender;
+        this.email = email;
+        this.nationality = nationality;
+        this.civilStatus = civilStatus;
     }
 
+    /**
+     * @return the name's person.
+     */
     public String getName() {
         return name;
+    }
+
+    /**
+     * @return the age's person.
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @return the CI's person.
+     */
+    public String getCI() {
+        return ci;
+    }
+
+    /**
+     * @return the height of the person.
+     */
+    public double getHeightMts() {
+        return heightMts;
+    }
+
+    /**
+     * @return the weight of the person.
+     */
+    public double getWeightKg() {
+        return weightKg;
+    }
+
+    /**
+     * @return the body mass index of the person.
+     */
+    public double getBodyMassIndex() {
+        return weightKg / (heightMts * heightMts);
+    }
+
+    /**
+     * @return the phone of the person.
+     */
+    public String getPhoneNumber() {
+        return phone;
+    }
+
+    /**
+     * @return the address of the person.
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @return the gender of the person.
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @return the email of the person.
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @return the nationality of the person.
+     */
+    public String getNationality() {
+        return nationality;
+    }
+
+    /**
+     * @return the civil status of the person.
+     */
+    public String getCivilStatus() {
+        return civilStatus;
     }
 }
--- quickstart/src/test/java/org/gradle/PersonTest.java
+++ quickstart/src/test/java/org/gradle/PersonTest.java
@@ -1,12 +1,115 @@
 package org.gradle;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
 
 public class PersonTest {
+    private Person person;
+
+    /**
+     * Initialize the person object.
+     */
+    @Before
+    public void initialize() {
+        person = new Person("franz", 25, "6666666", 2.0, 90.0,
+                "78547", "Av. ave", "male",
+                "franz@gmail.com", "Bolivian", "");
+    }
+
+    /**
+     * this method verifies the name of the person.
+     */
     @Test
-    public void canConstructAPersonWithAName() {
-        Person person = new Person("Larry");
-        assertEquals("Larry", person.getName());
+    public void testThePersonName() {
+        assertEquals("franz", person.getName());
+    }
+
+    /**
+     * this method verifies the age of the person.
+     */
+    @Test
+    public void testThePersonAge() {
+        assertEquals(25, person.getAge());
+    }
+
+    /**
+     * this method verifies the CI of the person.
+     */
+    @Test
+    public void testThePersonCI() {
+        assertEquals("6666666", person.getCI());
+    }
+
+    /**
+     * this method verifies the height of the person.
+     */
+    @Test
+    public void testThePersonHeight() {
+        assertEquals(2.0, person.getHeightMts(), 0.0);
+    }
+
+    /**
+     * this method verifies the weight of the person.
+     */
+    @Test
+    public void testThePersonWeight() {
+        assertEquals(90.0, person.getWeightKg(), 0.0);
+    }
+
+    /**
+     * this method verifies the body mass index of the person.
+     */
+    @Test
+    public void testPersonBodyMass() {
+        assertEquals(22.5, person.getBodyMassIndex(), 0.0);
+    }
+
+    /**
+     * this method verifies the phone number of the person.
+     */
+    @Test
+    public void testPersonPhoneNumber() {
+        assertEquals("78547", person.getPhoneNumber());
+    }
+
+    /**
+     * this method verifies the address of the person.
+     */
+    @Test
+    public void testPersonAddress() {
+        assertEquals("Av. ave", person.getAddress());
+    }
+
+    /**
+     * this method verifies the gender of the person.
+     */
+    @Test
+    public void testPersonGender() {
+        assertEquals("male", person.getGender());
+    }
+
+    /**
+     * this method verifies the email of the person
+     */
+    @Test
+    public void testPersonEmail() {
+        assertEquals("franz@gmail.com", person.getEmail());
+    }
+
+    /**
+     * this method verifies the nationality of the person
+     */
+    @Test
+    public void testPersonNationality() {
+        assertEquals("Bolivian", person.getNationality());
+    }
+
+    /**
+     * this method verifies the civil status of the person
+     */
+    @Test
+    public void testPersonCivilStatus() {
+        assertEquals("", person.getCivilStatus());
     }
 }
